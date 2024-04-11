a5be57b77f0c4bfeddbe239f85a04aa0266747a0
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-04-11 14:18:23 +0200

    added "use client"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { fetchStats } from "@/app/api/services";
 
 // Define the type for your stats here. This is a placeholder.
-// Adjust according to the actual structure of your stats object.
 type StatsType = any;
 
 export default function Home() {
@@ -14,9 +15,6 @@ export default function Home() {
     });
   }, []);
 
-  // It's a good practice to handle potential null or undefined values.
-  // Especially important if `stats` could be an object, array, etc.
-  // The following is a simple rendering example.
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {stats ? <p>{JSON.stringify(stats)}</p> : <p>Loading...</p>}
